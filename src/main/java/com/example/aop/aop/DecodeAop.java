993923af9f649bcb5d412e42283cfbed881a44e1
Reddyong <d0209hong@naver.com>
2024-01-30 16:51:04 +0900

    feat : aop practice

---
+++ src/main/java/com/example/aop/aop/DecodeAop.java
@@ -0,0 +1,48 @@
+package com.example.aop.aop;
+
+import com.example.aop.dto.User;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.Base64;
+
+@Aspect
+@Component
+public class DecodeAop {
+
+    @Pointcut("execution(* com.example.aop.controller..*.*(..))")
+    private void cut() {}
+
+    @Pointcut("@annotation(com.example.aop.annotation.Decode)")
+    private void enableDecode() {}
+
+    @Before("cut() && enableDecode()")
+    public void before(JoinPoint joinPoint) {
+
+        Object[] args = joinPoint.getArgs();
+
+        for (Object obj : args) {
+            if (obj instanceof User) {
+                User user = User.class.cast(obj);
+                String base64Email = user.getEmail();
+                String email = new String(Base64.getDecoder().decode(base64Email));
+                user.setEmail(email);
+            }
+        }
+    }
+
+    @AfterReturning(value = "cut() && enableDecode()", returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj) {
+
+        if (returnObj instanceof User) {
+            User user = User.class.cast(returnObj);
+            String email = user.getEmail();
+            String base64Email = Base64.getEncoder().encodeToString(email.getBytes());
+            user.setEmail(base64Email);
+        }
+    }
+}
